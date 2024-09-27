9fe16b2f52b80db1e8d0e79033fd8e8314a40265
Abimega200 <aabi16528@gmail.com>
2024-09-27 11:22:52 +0530

    helllo

---
+++ Routes/route.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  createProducts,
+  deleteProduct,
+  getProducts,
+  updateProduct,
+} from "../Controllers/controller.js";
+
+const router = express.Router();
+
+router.get("/", getProducts);
+router.post("/", createProducts);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+export default router;
+
+
